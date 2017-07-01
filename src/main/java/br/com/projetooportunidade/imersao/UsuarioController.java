b6aee9a5db2081c111ecdd787daedb5600e09e53
Luis <Luis@JUNIOR-NOTE>
2017-07-01 13:09:20 -0300

    Split do primeiro dia

--- src/main/java/br/com/projetooportunidade/imersao/UsuarioController.java
+++ src/main/java/br/com/projetooportunidade/imersao/UsuarioController.java
@@ -1,9 +1,14 @@
 package br.com.projetooportunidade.imersao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,8 +25,27 @@ public class UsuarioController {
 		return repository.findAll();
 	}
 	
-	public ResponseEntity<Usuario> add(@RequestBody Usuario u) {
-		
-		return null;
+	@PostMapping()
+	public @ResponseBody String add(@RequestBody Usuario usuario) {
+		repository.save(usuario);
+		return "Saved";
+	}
+	
+	@DeleteMapping(path="/{id}")
+	public @ResponseBody String remove(@PathVariable long id) {
+		repository.delete(id);
+		return "Removed";
+	}
+	
+	@GetMapping(path="/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable long id) {
+		Usuario usuario = repository.findOne(id);
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.CREATED);
+	}
+	
+	@PutMapping()
+	public ResponseEntity<Usuario> update(@RequestBody Usuario usuario) {
+		repository.save(usuario);
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.ACCEPTED);
 	}
 }
